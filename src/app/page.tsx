6c61de4d4320bb8facd8adfb5cb51a8996e1a3db
Elen Damares <elen.damares774@gmail.com>
2025-04-21 19:49:45 -0300

    link on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <p>Check the text editor <Link href={'/dashboard'} className="text-blue-800 cursor-pointer underline">here</Link></p>
+    <div className='w-full h-full flex justify-center items-center text-[2rem]'>
+        <p>Check the text editor <Link href={'/dashboard'} className="text-blue-800 cursor-pointer underline">here</Link></p>
+    </div>
   );
 }
